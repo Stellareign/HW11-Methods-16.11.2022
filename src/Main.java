88230d7a542c622031020ce1a11e36023cf8bb38
Stellareign <tuguran@mail.ru>
2022-11-19 20:02:43 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner; // импортировали сканнер
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) { // хотелось, но не получилось Scanner вынести в метод, путаница с переменой year получается
         System.out.println("Домашка 11, Методы, задача 1:");
         Scanner scanner = new Scanner(System.in);// объявили переменную scanner с типом
         System.out.println("Введите год:");
@@ -9,13 +8,15 @@ public class Main {
         System.out.println(leapOrNoLeapYear(year));
         main(args); // вызываем программу заново2
     }
-    private static String leapOrNoLeapYear(int year2){
+
+    private static String leapOrNoLeapYear(int year2) {
         int leapYear1 = year2 % 4;
         int leapYear2 = year2 % 400;
         int noLeapYear = year2 % 100;
         if (noLeapYear != 0 && leapYear1 == 0 || leapYear2 == 0) {
-        return  "Этот год високосный.";
+            return "Этот год високосный.";
         }
         return "Этот год не високосный."; // я так и не поняла до конца, как у меня это получилось
     }
+
 }
